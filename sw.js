9b71d58d94cdce91074e76a4b49ee5165e7f8d98
Karnx630 <karansuthar6300@gmail.com>
2026-03-21 21:42:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,22 @@
-const CACHE_NAME = 'invoice-v5';
+const CACHE_NAME = 'invoice-pro-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json'
+];
 
-self.addEventListener('install', e => { 
-    self.skipWaiting(); 
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(c => c.addAll([
-            './',
-            './index.html',
-            './manifest.json'
-        ]))
-    ); 
+// Install event: cache all necessary files
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener('activate', e => { 
-    e.waitUntil(
-        caches.keys().then(ks => Promise.all(
-            ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-        ))
-    ); 
-});
-
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
+// Fetch event: serve from cache if offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
     );
 });
